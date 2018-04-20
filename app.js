d387dc46dc64b4f35a241f6b129615f980ca229c
陈春勇 <815472970@qq.com>
2018-04-20 17:43:57 +0800

    删除空格

--- app.js
+++ app.js
@@ -5,16 +5,13 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end(`remoteIP:${getClientIp(req)},localhost:${ip.address()}`);
 });
-
 server.listen(3000, '0.0.0.0', () => {
     console.log(`Server running at 3000 `);
 });
-
 function getClientIp(req) {
     let ip = req.headers['x-forwarded-for'] || req.ip ||req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress || '';
     if (ip.split(',').length > 0) {
         ip = ip.split(',')[0]
     }
     return ip;
-
 };
